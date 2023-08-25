3a50c644eda1887509c4b80dae3de6b304b52f18
NadyaGus <ftotkexit@gmail.com>
2023-08-25 23:00:26 +0300

    feat: add two if/else

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@
 
 module.exports = function towelSort (matrix) {
   
-  
-
-  matrix[0].sort((a, b) => (a - b));
-  matrix[1].sort((a, b) => (b - a));
-  matrix[2].sort((a, b) => (a - b));
-  
+  if (matrix === undefined) {
+    return [];
+  } else if (matrix.length === 0) {
+    return [];
+  } else {
+    matrix[0].sort((a, b) => (a - b));
+    matrix[1].sort((a, b) => (b - a));
+    matrix[2].sort((a, b) => (a - b));
     return matrix.flat();
   }
+  }
+
+
+  /*
+  попробовать сделать новый массив
+
+  */
+  
